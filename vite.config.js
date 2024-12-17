8d33657916fa1cab63d71122ff814ca8e7ad6a6d
Armond Schneider <mondoloves@icloud.com>
2024-12-17 10:07:39 -0700

    changes for pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ghPages } from "vite-plugin-gh-pages";
 
-// Replace "your-username/your-repo-name" with your GitHub repository name.
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: "/coffee-site/", // Base path for GitHub Pages
+  base: "/coffee-site/",
 });
